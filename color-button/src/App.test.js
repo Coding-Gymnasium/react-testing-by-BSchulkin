c902d1d119eb100fa5cdbbdd92e57de82504ce49
Nico Rithner <nicorithner@gmail.com>
2021-11-04 14:28:37 -0600

    Update tests and code to change color

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -7,7 +7,7 @@ let checkboxOne;
 
 beforeEach(() => {
   render(<App />);
-  colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  colorButton = screen.getByRole('button', { name: 'Change to Midnight Blue' });
   checkbox = screen.getByRole('checkbox');
   checkboxOne = screen.getByRole('checkbox', {
     name: 'Disable button',
@@ -16,17 +16,17 @@ beforeEach(() => {
 
 describe('Test Change color button functionality', () => {
   it('button has the correct initial color', () => {
-    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
   });
 
-  it('button turns blue when clicked', () => {
-    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+  it('button turns MidnightBlue when clicked', () => {
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
 
     fireEvent.click(colorButton);
 
-    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
 
-    expect(colorButton.textContent).toBe('Change to red');
+    expect(colorButton.textContent).toBe('Change to Medium Violet Red');
   });
 });
 
@@ -47,25 +47,25 @@ describe('Test checkbox toggles button', () => {
     expect(colorButton).toBeEnabled();
   });
 
-  it('Checkbox changes button color to gray when disabled and to blue when enabled', () => {
+  it('Checkbox changes button color to gray when disabled and to MidnightBlue when enabled', () => {
     fireEvent.click(colorButton);
-    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
 
     fireEvent.click(checkboxOne);
     expect(colorButton).toHaveStyle({ backgroundColor: 'gray' });
 
     fireEvent.click(checkboxOne);
-    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
   });
 
-  it('Checkbox changes button color to gray when disabled and to red when enabled', () => {
-    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+  it('Checkbox changes button color to gray when disabled and to MediumVioletRed when enabled', () => {
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
 
     fireEvent.click(checkboxOne);
     expect(colorButton).toHaveStyle({ backgroundColor: 'gray' });
 
     fireEvent.click(checkboxOne);
-    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+    expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
   });
 });
 
