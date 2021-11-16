ae7410a63f810d7000fdf4f3847297eb5b6a4b4b
Nico Rithner <nicorithner@gmail.com>
2021-11-16 09:06:32 -0700

    Create Entry directory
    Create Options.js
    Create ScoopOption.js
    Start test

---
+++ sundaes-on-demand/src/pages/entry/test/Options.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+
+import Options from '../Options';
+
+describe('Tests Options module features', () => {
+  it('can find the images', () => {
+    render(<Options optionType="scoopt" />);
+
+    // find images
+    const scoopImages = screen.getAllByRole('img', {
+      name: /scoop$/i,
+    });
+
+    expect(scoopImages).toHaveLength(2);
+
+    // confirm alt text of images
+    const altText = scoopImages.map(element => element.alt);
+
+    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+  });
+
+  it('displays image for each scoop option from server', () => {});
+});
