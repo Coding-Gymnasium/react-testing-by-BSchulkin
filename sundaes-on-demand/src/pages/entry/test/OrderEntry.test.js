9bcb4e9fb16ad4095ca7d8c1511ac381d4b7f5d9
Nico Rithner <nicorithner@gmail.com>
2021-11-19 22:23:27 -0700

    Create OrderEntry test

---
+++ sundaes-on-demand/src/pages/entry/test/OrderEntry.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+
+import { OrderEntry } from '../OrderEntry';
+import { rest } from 'msw';
+
+import { server } from '../../../mocks/server';
+
+it('handles error for scoops and topping routes', async () => {
+  server.resetHandlers(
+    rest.get('http://localhost:3000/scoops', (req, res, ctx) => {
+      res(ctx.status(500));
+    }),
+    rest.get('http://localhost:3000/toppings', (req, res, ctx) => {
+      res(ctx.status(500));
+    }),
+  );
+
+  render(<OrderEntry />);
+
+  const alerts = await screen.findAllByRole('alert', {
+    name: 'An unexpected error occurred. Please try again later',
+  });
+
+  expect(alerts).toHaveLength(2);
+});
