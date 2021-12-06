84ed667d64296ba583b75c590122caaeef3626b8
Nico Rithner <nicorithner@gmail.com>
2021-12-05 19:55:37 -0700

    Complete lesson 61

--- sundaes-on-demand/src/App.js
+++ sundaes-on-demand/src/App.js
@@ -1,9 +1,20 @@
+import { Container } from 'react-bootstrap';
+import OrderEntry from './pages/entry/OrderEntry';
+import { OrderDetailsProvider } from './context/OrderDetails';
+
 import './App.css';
 
-import { SummaryForm } from '../src/pages/summary/SummaryForm';
-
 function App() {
-  return <SummaryForm />;
+  return (
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+
+      {/* Confirmation page does not need provider */}
+    </Container>
+  );
 }
 
 export default App;
--- sundaes-on-demand/src/pages/entry/Options.js
+++ sundaes-on-demand/src/pages/entry/Options.js
@@ -5,10 +5,13 @@ import Row from 'react-bootstrap/Row';
 import ScoopOption from '../entry/ScoopOption';
 import ToppingOption from './ToppingOption';
 import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../context/OrderDetails';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -27,12 +30,27 @@ export default function Options({ optionType }) {
   const ItemComponent =
     optionType === 'scoops' ? ScoopOption : ToppingOption;
 
+  const title =
+    optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
   const optionItems = items.map(item => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
     />
   ));
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {total} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
